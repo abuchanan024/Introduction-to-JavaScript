ddba85bf544d4061f0375d08e40ccc9c7bff0302
antoniobuchanan24 <90158275+antoniobuchanan24@users.noreply.github.com>
2021-11-02 16:37:34 -0700

    done with the first three

--- index.js
+++ index.js
@@ -86,9 +86,10 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
-}
+function dogYears(humanYears){
+  return humanYears * 7
+};
+console.log(dogYears(1));
 
 
 
@@ -178,9 +179,11 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
-}
+function miles(km){
+  return km * 1.60934
+};
+
+console.log(miles(5));
 
 
 
